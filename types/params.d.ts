fcca0a10c48e4403c20cd3254d1a64194c2f8b0b
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2026-01-27 15:15:54 +0000

    Create param types to provide type information and help users to create a valid query paramater object
    Sourced from the guardian's docs on the search/content
    endpoint and all the options and possible values for each.

---
+++ types/params.d.ts
@@ -0,0 +1,98 @@
+// Types to encapsulate the values accepted by the api for each query parameter.
+type Format = 'json' | 'xml';
+type SortOrder = 'newest' | 'oldest' | 'relevance';
+type OrderDate = 'published' | 'newspaper-edition' | 'last-modified';
+type UseDate = OrderDate | 'first-publication';
+type ShowField =
+  | 'trailText'
+  | 'headline'
+  | 'showInRelatedContent'
+  | 'body'
+  | 'lastModified'
+  | 'hasStoryPackage'
+  | 'score'
+  | 'standFirst'
+  | 'shortUrl'
+  | 'thumbnail'
+  | 'wordcount'
+  | 'commentable'
+  | 'isPremoderated'
+  | 'allowUgc'
+  | 'byline'
+  | 'publication'
+  | 'internalPageCode'
+  | 'productionOffice'
+  | 'shouldHideAdverts'
+  | 'liveBloggingNow'
+  | 'commentCloseDate'
+  | 'starRating'
+  | 'all';
+type ShowTag =
+  | 'blog'
+  | 'contributor'
+  | 'keyword'
+  | 'newspaper-book'
+  | 'newspaper-book-section'
+  | 'publication'
+  | 'series'
+  | 'tone'
+  | 'type'
+  | 'all';
+type ShowElement = 'audio' | 'image' | 'video' | 'all';
+type ShowReference =
+  | 'author'
+  | 'bisac-prefix'
+  | 'esa-cricket-match'
+  | 'esa-football-match'
+  | 'esa-football-team'
+  | 'esa-football-tournament'
+  | 'isbn'
+  | 'imdb'
+  | 'musicbrainz'
+  | 'musicbrainzgenre'
+  | 'opta-cricket-match'
+  | 'opta-football-match'
+  | 'opta-football-team'
+  | 'opta-football-tournament'
+  | 'pa-football-competition'
+  | 'pa-football-match'
+  | 'pa-football-team'
+  | 'r1-film'
+  | 'reuters-index-ric'
+  | 'reuters-stock-ric'
+  | 'witness-assignment';
+type ShowRight = 'syndicatable' | 'subscription-databases' | 'all';
+
+// Interfaces to encapsulate the accepted fields by each api endpoint.
+interface ApiParams {
+  format?: Format;
+  callback?: string;
+}
+
+interface SearchParams extends ApiParams {
+  q?: string;
+  queryFields?: Array<string>;
+  section?: string;
+  reference?: string;
+  referenceType?: string;
+  tag?: string;
+  rights?: string;
+  ids?: string;
+  productionOffice?: string;
+  lang?: string;
+  starRating?: number;
+  fromDate?: Date;
+  toDate?: Date;
+  useDate?: UseDate;
+  page?: number;
+  pageSize?: number;
+  orderBy?: SortOrder;
+  orderDate?: OrderDate;
+  showFields?: Array<ShowField>;
+  showTags?: Array<ShowTag>;
+  showSection?: boolean;
+  showBlocks?: string; // Too complicated to try and make into a type?
+  showElements?: Array<ShowElement>;
+  showReferences?: Array<ShowReference>;
+  showRights?: Array<ShowRight>;
+}
