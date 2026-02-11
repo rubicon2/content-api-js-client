e69e39fd37c16036b15cd11d9c10a065deb8b471
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2026-02-11 14:07:00 +0000

    Add jsdoc comments to client

--- src/client.ts
+++ src/client.ts
@@ -17,14 +17,23 @@ import type {
 } from './api.js';
 import { paramsToStr } from './params.js';
 
+/**
+ * A client to interface with the Guardian's content API.
+ */
 class Client {
   #apiKey: string;
   #baseUrl: string = 'https://content.guardianapis.com';
 
+  /**
+   * @param {string} apiKey Your API key, which can be acquired from here: {@link https://open-platform.theguardian.com/access}
+   */
   constructor(apiKey: string) {
     this.#apiKey = apiKey;
   }
 
+  /**
+   * A private function to simplify the fetch process and sanitize the url.
+   */
   async #apiFetch<ReturnType>(
     url: string,
     params:
@@ -49,11 +58,20 @@ class Client {
     }
   }
 
+  /**
+   * Retrieve an individual item by its id.
+   * @param {string} id The id of the item to be retrieved.
+   * @param {ContentParams} params The parameters of the query. See {@link https://open-platform.theguardian.com/documentation/item} for details.
+   */
   async item(id: string, params: ContentParams = {}): Promise<Content> {
     const data: ApiResponseSingle<Content> = await this.#apiFetch(id, params);
     return data.content;
   }
 
+  /**
+   * Retrieve items based on query parameters.
+   * @param {QueryContentParams} params The parameters of the query. See {@link https://open-platform.theguardian.com/documentation/search} for details.
+   */
   async search(params: QueryContentParams = {}): Promise<Array<Content>> {
     const data: ApiSearchResponse<Content> = await this.#apiFetch(
       'search',
@@ -62,6 +80,15 @@ class Client {
     return data.results;
   }
 
+  /**
+   * Retrieve the next set of items based on query parameters, and after the provided id.
+   * This method allows for deep pagination, unlike the page/pageSize fields, which can only
+   * page through several thousand results. Simply provide the id of the last item in the
+   * previous set of results (from a search or previous calls to next), and use the same params
+   * to page through the results.
+   * @param {string} id The id of the last item retrieved.
+   * @param {QueryContentParams} params The parameters of the query. See {@link https://open-platform.theguardian.com/documentation/search} for details.
+   */
   async next(
     id: string,
     params: QueryContentParams = {},
@@ -73,11 +100,19 @@ class Client {
     return data.results;
   }
 
+  /**
+   * Retrieve tags based on query parameters.
+   * @param {QueryTagParams} params The parameters of the query. See {@link https://open-platform.theguardian.com/documentation/tag} for details.
+   */
   async tags(params: QueryTagParams = {}): Promise<Array<Tag>> {
     const data: ApiResponseMultiple<Tag> = await this.#apiFetch('tags', params);
     return data.results;
   }
 
+  /**
+   * Retrieve sections based on query parameters.
+   * @param {QuerySectionParams} params The parameters of the query. See {@link https://open-platform.theguardian.com/documentation/section} for details.
+   */
   async sections(params: QuerySectionParams = {}): Promise<Array<Section>> {
     const data: ApiResponseMultiple<Section> = await this.#apiFetch(
       'sections',
@@ -86,6 +121,10 @@ class Client {
     return data.results;
   }
 
+  /**
+   * Retrieve editions based on query parameters.
+   * @param {QueryEditionParams} params The parameters of the query. See {@link https://open-platform.theguardian.com/documentation/edition} for details.
+   */
   async editions(params: QueryEditionParams = {}): Promise<Array<Edition>> {
     const data: ApiResponseMultiple<Edition> = await this.#apiFetch(
       'editions',
