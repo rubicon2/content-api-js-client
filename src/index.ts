1cff0b71c3f3736dda71cae22346549c5487d6ec
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2026-02-21 14:29:47 +0000

    Export types from index so consumers can use
    E.g. a user might want a re-usable query object,
    for use in search and next endpoints. By using
    an exported QueryContentParams interface, they can
    now type that object and get suggestions and type
    checking.

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,42 @@
+export type {
+  SortOrder,
+  OrderDate,
+  UseDate,
+  FieldName,
+  TagName,
+  ElementName,
+  ReferenceName,
+  RightName,
+  QueryItemParams,
+  QueryContentParams,
+  QueryTagParams,
+  QuerySectionParams,
+  QueryEditionParams,
+} from './params.js';
+export type {
+  ApiResponseMeta,
+  ApiPagedResponseMeta,
+  Content,
+  ContentBlock,
+  ContentElement,
+  ContentTag,
+  ElementAsset,
+  Sponsorship,
+  Tag,
+  Section,
+  SectionEdition,
+  Edition,
+} from './api.js';
+export type {
+  ClientSuccess,
+  ClientError,
+  ClientItemResponse,
+  ClientSearchResponse,
+  ClientNextResponse,
+  ClientTagsResponse,
+  ClientSectionsResponse,
+  ClientEditionsResponse,
+} from './client.js';
+
 import Client from './client.js';
 export default Client;
