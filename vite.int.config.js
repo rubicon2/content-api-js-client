17762d16d2cca99c65253a0cf498b0e1423b540b
rubicon2 <41235563+rubicon2@users.noreply.github.com>
2026-03-02 14:25:50 +0000

    Add integration tests for examples in README

--- vite.int.config.js
+++ vite.int.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   test: {
-    include: 'test/int.test.js',
+    include: ['test/int.test.js', 'test/readme.test.ts'],
     fileParallelism: false,
   },
 });
